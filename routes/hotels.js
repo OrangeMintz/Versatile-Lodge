b994450350126d2e0629a2cedccccb14277494ac
OrangeMintz <Nickzgacus@gmail.com>
2023-09-24 13:15:44 +0800

    added put method

--- routes/hotels.js
+++ routes/hotels.js
@@ -13,9 +13,20 @@ router.post("/", async (req, res) => {
     }catch(err){
         res.status(500).json(err)
     }
-})
+});
+
+
 //Delete
 //Update
+router.put("/:id", async (req, res) => {
+    const newHotel = new Hotel(req.body)
+    try{
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body});
+        req.status(200).json(updatedHotel)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 //Get
 //GetAll
 
