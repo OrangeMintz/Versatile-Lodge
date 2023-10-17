d043b228618d1743a5baa21b8b8a7c2529f99661
ramonPL14 <2101102020@student.buksu.edu.ph>
2023-10-18 01:13:07 +0800

    add register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Home } from "./pages/home/Home";
 import List from "./pages/list/List";
 import About from "./pages/about/About";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/room" element={<List/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
 
 
         
