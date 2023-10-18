f6f91cd75dd269ad42e4fcd29028e6fcb87beda4
ramonPL14 <2101102020@student.buksu.edu.ph>
2023-10-18 18:35:10 +0800

    transfer main css of login and register

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import './login.css'
 
 export const Login = () => {
   return (
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import './register.css'
 
 export const Register = () => {
   return (
