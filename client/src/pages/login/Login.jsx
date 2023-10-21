bf5f7fde1682784101b0fab66b2df8f4f0683811
ramonPL14 <2101102020@student.buksu.edu.ph>
2023-10-22 06:54:19 +0800

    updated log-in

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -5,45 +5,40 @@ export const Login = () => {
   return (
     <Fragment>
         
-        <div className="container">
-
-        <div className="contain">
-
-            <div className="links">
-                <h3 className="title">Log-in with</h3>
-                <div className="buttons">
-                    <a href="" className="btn"><i className="fab fa-google"></i>google</a>
-                    <a href="" className="btn"><i className="fab fa-facebook"></i>facebook</a>
-                </div>
+    <div className="login-container">
+        <div className="image">
+            <img src="assets/images/chizuru.jpg" alt="Lodge Logo" />
+            <p className="centered-text">Versatile Lodge</p>
+        </div>
+        <div className="form">
+            <h3>Log-in with</h3>
+            <div className="buttons">
+                <a href="" className="btn"><i className="fab fa-google"></i>google</a>
+                <a href="" className="btn"><i className="fab fa-facebook"></i>facebook</a>
             </div>
 
-
-            <form action="/">
-
-                <div className="input-box">
-                    <h3 className="title">or sign in with email</h3>
-                    <div className="input">
-                        <span className="far fa-envelope"></span>
-                        <input type="email" name="" placeholder="email" id="" />
-                    </div>
-                    <div className="input">
-                        <span className="fas fa-lock"></span>
-                        <input type="password" name="" placeholder="password" id="" />
-                    </div>
-                    <div className="checkbox">
-                        <input type="checkbox" name="" id="remember" />
-                        <label for="remember">remember me</label>
-                    </div>
+            <div className="input-box">
+                <h3 className="title">or sign in with email</h3>
+                <div className="input">
+                    <span className="far fa-envelope"></span>
+                    <input type="email" name="" placeholder="email" id="" />
+                </div>
+                <div className="input">
+                    <span className="fas fa-lock"></span>
+                    <input type="password" name="" placeholder="password" id="" />
+                </div>
+                <div className="checkbox">
+                    <input type="checkbox" name="" id="remember" />
+                    <label for="remember">remember me</label>
+                </div>
+                <div className="buttons">
                     <input type="submit" value="Login" className="log-in-btn" />
                     <h3 className="title">Don't have an account?</h3>
                     <a className="register-btn">Register</a>
                 </div>
-
-            </form> 
-
+            </div>
         </div>
-
-        </div>
+    </div>
 
     
 
