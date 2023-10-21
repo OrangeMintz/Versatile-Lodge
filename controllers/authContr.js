225d7251576bd93047893e95ed8c66b8ae3cd578
OrangeMintz <Nickzgacus@gmail.com>
2023-10-21 20:41:57 +0800

    added axios

--- controllers/authContr.js
+++ controllers/authContr.js
@@ -1,70 +1,95 @@
-const User = require('../models/User.js')
-const bcrypt = require('bcryptjs');
-const createError = require('../utils/error.js');
-const jwt = require('jsonwebtoken');
+const User = require("../models/User.js");
+const Customer = require("../models/Customer.js");
+const bcrypt = require("bcryptjs");
+const createError = require("../utils/error.js");
+const jwt = require("jsonwebtoken");
 
+const register = async (req, res, next) => {
+  try {
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(req.body.password, salt);
 
-const register = async(req, res, next) =>{
-    try{
-        var salt = bcrypt.genSaltSync(10);
-        var hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      name: req.body.name,
+      password: hash,
+      email: req.body.email,
+      phonenumber: req.body.phonenumber,
+      address: req.body.address,
+      birthday: req.body.birthday,
+      isAdmin: req.body.isAdmin,
+      isEmployee: req.body.isEmployee,
+      sex: req.body.sex,
+    });
 
-        const newUser = new User({
-            username: req.body.username,
-            password: hash,
-            email: req.body.email,
-            phonenumber: req.body.phonenumber,
-            address: req.body.address,
-            birthday: req.body.birthday,
-            isAdmin: req.body.isAdmin,
-            isEmployee: req.body.isEmployee,
-            sex: req.body.sex
-        })
+    const regUser = await newUser.save();
+    res.status(200).json(regUser);
+  } catch (err) {
+    next(err);
+  }
+};
 
-        const regUser = await newUser.save()
-        res.status(200).json(regUser);
-        
-    }catch(err){
-        next(err)
-    }
-}
+const registerCust = async (req, res, next) => {
+  try {
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(req.body.password, salt);
 
-const login = async(req, res, next) =>{
-    try{
+    const newUserCust = new Customer({
+      name: req.body.name,
+      password: hash,
+      email: req.body.email,
+      photo: req.body.photo,
+    });
+
+    const regUserCust = await newUserCust.save();
+    res.status(200).json(regUserCust);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const login = async (req, res, next) => {
+  try {
     const user = await User.findOne({
-        username: req.body.username
-    })
-    if(!user) 
-    return next(createError(404, "User not found!"));
+      username: req.body.username,
+    });
+    if (!user) return next(createError(404, "User not found!"));
 
-    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
-    if(!isPasswordCorrect) 
-    return next(createError (400, "Wrong Password or Username!"));
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect)
+      return next(createError(400, "Wrong Password or Username!"));
 
     // success
-    const token = jwt.sign({
-        id: user._id, isAdmin: user.isAdmin },
-        process.env.JWT)
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT
+    );
 
-    const {password, ...otherDetails} = user._doc;
-    res.cookie("access_token", token, {
+    const { password, ...otherDetails } = user._doc;
+    res
+      .cookie("access_token", token, {
         httpOnly: true,
-    }).status(200).json({otherDetails});
-        
-    }catch(err){
-        next(err);
-    }
-} 
+      })
+      .status(200)
+      .json({ otherDetails });
+  } catch (err) {
+    next(err);
+  }
+};
 
+const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
 
-
-const getUser = async(req, res, next) =>{
-    try{
-        const user = await User.findById(req.params.id);
-        res.status(200).json(user)
-    }catch(err){
-        next(err)
-    }
-} 
-
-module.exports = {register, getUser, login}
+module.exports = { register, getUser, login, registerCust };
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const {register, getUser, login} = require('../controllers/authContr.js')
+const express = require("express");
+const {
+  register,
+  getUser,
+  login,
+  registerCust,
+} = require("../controllers/authContr.js");
 const router = express.Router();
 
-
 router.post("/register", register);
+router.post("/register/customer", registerCust);
 router.post("/login", login);
 router.get("/user/:id", getUser);
 
-
- 
 module.exports = router;
