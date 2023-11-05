8e79ecf8a0470a308eb718157ad3d8da16fe27a7
OrangeMintz <Nickzgacus@gmail.com>
2023-11-05 14:53:51 +0800

    done fetching rooms and room details

--- controllers/roomContr.js
+++ controllers/roomContr.js
@@ -68,7 +68,8 @@ const updateRoom = async (req, res, next) => {
 const getRoom = async (req, res, next) => {
     try {
         const room = await Room.findById(req.params.id);
-        res.status(200).json(room)
+        res.send(room)
+        // res.status(200).json(room)
     } catch (err) {
         next(err)
     }
