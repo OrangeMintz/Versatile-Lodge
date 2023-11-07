328632647e025af60aa55a5ba286035eae36caf2
ramonPL14 <2101102020@student.buksu.edu.ph>
2023-11-07 14:46:40 +0800

    added change profile picture

--- client/src/pages/bookingHistory/BookingHistory.jsx
+++ client/src/pages/bookingHistory/BookingHistory.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import "./bookingHistory.css"
+import { Link } from "react-router-dom";
 import Footer from '../../component/footer';
 
 const BookingHistory = () => {
