f549e11eb163a0ff800adcd8b35927ee0d2fd6b9
OrangeMintz <Nickzgacus@gmail.com>
2023-11-12 00:26:05 +0800

    stores google account to database

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -48,15 +48,6 @@ function Navbar() {
         return location.pathname === pathname ? 'active' : '';
     };
 
-
-    const cld = new Cloudinary({
-        cloud: {
-            cloudName: 'dl0qncxjh'
-        }
-    });
-
-    // const myImage = cld.image(user.image);
-
     return (
         <div>
             <section className="header">
@@ -108,3 +99,122 @@ function Navbar() {
 }
 
 export default Navbar;
+
+
+
+
+
+
+
+
+
+// import React, { useContext, useEffect, useState } from 'react';
+// import { Link, useLocation } from 'react-router-dom';
+// import { UserContext } from "../context/userContext.jsx";
+// import axios from 'axios';
+
+// import { AdvancedImage } from '@cloudinary/react';
+// import { Cloudinary } from "@cloudinary/url-gen";
+// import { Transformation } from "@cloudinary/url-gen";
+
+// function Navbar() {
+//     const { user, setUser } = useContext(UserContext);
+//     const location = useLocation();
+
+//     useEffect(() => {
+//         if (!user) {
+//             axios.get('/profile')
+//                 .then(({ data }) => {
+//                     setUser(data);
+//                 })
+//                 .catch((error) => {
+//                     console.error('Error fetching user profile:', error);
+//                 });
+//         }
+//     }, [user, setUser]);
+
+//     const handleLogout = () => {
+//         axios.get('/logout')
+//             .then(() => {
+//                 window.location.href = `${window.location.origin}/`;
+//             })
+//             .catch((error) => {
+//                 console.error('Error during logout:', error);
+//             });
+//     };
+
+//     const [navbarActive, setNavbarActive] = useState(false);
+
+//     const toggleNavbar = () => {
+//         setNavbarActive(!navbarActive);
+//     };
+
+//     const handleUserBtnClick = () => {
+//         const profile = document.querySelector('.profile');
+//         profile.classList.toggle('active');
+//     };
+
+//     const isLinkActive = (pathname) => {
+//         return location.pathname === pathname ? 'active' : '';
+//     };
+
+
+//     const cld = new Cloudinary({
+//         cloud: {
+//             cloudName: 'dl0qncxjh'
+//         }
+//     });
+
+//     // const myImage = cld.image(user.image);
+
+//     return (
+//         <div>
+//             <section className="header">
+//                 <div className="flex">
+//                     <Link to="/" className="logo">Versatile Lodge</Link>
+//                     <div className="right-buttons">
+//                         <Link to="/room" className="btn">Check Availability</Link>
+//                         {!user && (
+//                             <Link to="/login" className="btn" style={{ marginLeft: '10px', border: '0px' }}>Login/Register</Link>
+//                         )}
+//                     </div>
+
+//                     <div
+//                         className={`menu fas fa-bars ${navbarActive ? 'active' : ''}`}
+//                         id="menu-btn"
+//                         onClick={toggleNavbar}
+//                     ></div>
+//                 </div>
+
+//                 <nav className={`navbar ${navbarActive ? 'active' : ''}`}>
+//                     <Link to="/" className={isLinkActive('/')}>Home</Link>
+//                     <Link to="/about" className={isLinkActive('/about')}>About</Link>
+//                     <Link to="/room" className={isLinkActive('/room')}>Rooms</Link>
+//                     <Link to="/contact" className={isLinkActive('/contact')}>Contact</Link>
+//                     <Link to="/reviews" className={isLinkActive('/reviews')}>Reviews</Link>
+//                     {user && (
+//                         <div>
+//                             <img src={user.image} id="user-btn" alt="user" onClick={handleUserBtnClick} />
+//                             <div className="profile">
+//                                 <img src={user.image} alt="" />
+//                                 {!!user && <h3>{user.name}</h3>}
+//                                 <span>Client</span>
+//                                 <Link to={`/accountSetting/${user.id}`} className='btn'>View Profile</Link>
+//                                 <div className="flex-btn">
+//                                     <a href={`/bookingHistory/${user.id}`} className="option-btn">
+//                                         History
+//                                     </a>
+//                                     <Link className="option-btn" onClick={handleLogout}>
+//                                         Logout
+//                                     </Link>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     )}
+//                 </nav>
+//             </section>
+//         </div>
+//     );
+// }
+
+// export default Navbar;
--- server/routes/request.js
+++ server/routes/request.js
@@ -2,29 +2,31 @@ var express = require('express');
 var router = express.Router();
 const dotenv = require('dotenv');
 dotenv.config(); // Load environment variables from .env file
-const {OAuth2Client} = require('google-auth-library');
+const { OAuth2Client } = require('google-auth-library');
 
 /* GET users listing. */
-router.post('/', async function(req, res, next) {
+router.post('/', async function (req, res, next) {
   res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
   res.header("Access-Control-Allow-Credentials", 'true');
-  res.header("Referrer-Policy","no-referrer-when-downgrade");
+  res.header("Referrer-Policy", "no-referrer-when-downgrade");
+
   const redirectURL = 'http://127.0.0.1:8000/oauth';
+  // const redirectURL = 'http://localhost:3000/oauth';
 
   const oAuth2Client = new OAuth2Client(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
-      redirectURL
-    );
+    redirectURL
+  );
 
-    // Generate the url that will be used for the consent dialog.
-    const authorizeUrl = oAuth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid',
-      prompt: 'consent' 
-    });
+  // Generate the url that will be used for the consent dialog.
+  const authorizeUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid',
+    prompt: 'consent'
+  });
 
-    res.json({url:authorizeUrl})
+  res.json({ url: authorizeUrl })
 
 });
 
