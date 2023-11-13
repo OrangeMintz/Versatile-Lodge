3eaeced01131c755e959c3e3eddc928565f5ef34
OrangeMintz <Nickzgacus@gmail.com>
2023-11-13 23:33:08 +0800

    almost done with booking

--- client/src/pages/unauthorized/unauthorized.jsx
+++ client/src/pages/unauthorized/unauthorized.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../../component/Navbar";
 import Footer from "../../component/footer";
+import "./unathorized.css";
 
 
 
@@ -18,12 +19,18 @@ const Unauthorized = () => {
     return (
         <>
             <Navbar />
-            <div style={{ textAlign: 'center', marginTop: '10%', marginBottom: ' 10%' }}>
-                <h1 style={{ fontSize: '3em', color: 'wheat' }}>Unauthorized</h1>
-                <p style={{ fontSize: '1.5em', color: 'wheat' }}>You don't have permission to access this page.</p>
-                <button onClick={handleGoBack} style={{ fontSize: '1.2em', marginTop: '20px' }}>
-                    Go Back Home
-                </button>
+            <div className="wrapper">
+                <div style={{ textAlign: 'center', maxWidth: '600px' }}>
+                    <h1 style={{ fontSize: '13em', color: '#2B1103' }}>404</h1>
+                    <p style={{ fontSize: '3em', color: '#2B1103' }}>Not Found</p>
+                    <p style={{ fontSize: '2em', color: '#2B1103' }}>
+                        This resource request could not be found on this server!
+                    </p>
+
+                    <button className="btn-back" onClick={handleGoBack} style={{ fontSize: '1.2em', marginTop: '20px' }}>
+                        Go Back Home
+                    </button>
+                </div>
             </div>
             <Footer />
         </>
