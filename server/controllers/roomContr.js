9f0bd2b0eee9c67a5ea3c478fd9852cacdd9035e
OrangeMintz <Nickzgacus@gmail.com>
2023-11-14 23:44:41 +0800

    done with calendar filtering on booking

--- server/controllers/roomContr.js
+++ server/controllers/roomContr.js
@@ -86,14 +86,6 @@ const getRooms = async (req, res, next) => {
     }
 };
 
-// const getRooms = async (req, res, next) => {
-//     try {
-//         const rooms = await Room.find();
-//         res.status(200).json(rooms)
-//     } catch (err) {
-//         next(err)
-//     }
-// };
 
 
 
