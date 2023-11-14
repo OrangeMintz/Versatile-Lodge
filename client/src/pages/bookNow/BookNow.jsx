b276632eadd7cf76943fcfd9a3c7bb007f6f8bcd
OrangeMintz <Nickzgacus@gmail.com>
2023-11-14 19:18:53 +0800

    added booking, need design fix on BookNow.jsx

--- client/src/pages/bookNow/BookNow.jsx
+++ client/src/pages/bookNow/BookNow.jsx
@@ -8,24 +8,42 @@ import { useContext, useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import useFetch from "../../hooks/useFetch";
 import axios from 'axios';
-
-import moment from 'moment'
-
+import moment from 'moment';
+import Error from '../../component/Error';
 
 const BookNow = () => {
-
-    // Get Room
     const { id, fromDate, toDate } = useParams();
     const apiUrl = `http://localhost:8000/api/room/${id}`;
     const { data, loading, error } = useFetch(apiUrl);
-
-    // Get Profile
     const { user, setUser } = useContext(UserContext);
 
-    // Total Days
-    const totalDays = moment(toDate).diff(moment(fromDate), 'days') + 1;
+    const [isValidDates, setIsValidDates] = useState(true);
+
+    const totalDays = isValidDates ? moment(toDate).diff(moment(fromDate), 'days') + 1 : 0;
     const totalAmount = totalDays * data.price;
 
+    async function bookRoom() {
+        const bookingDetails = {
+            room: data.name,
+            room_id: data._id,
+            user_id: user && user.id,
+            fromDate,
+            toDate,
+            totalAmount,
+            totalDays,
+            transaction_id: 124131
+        }
+
+        try {
+            const result = await axios.post('/api/booking/', bookingDetails)
+            console.log(result)
+        } catch (error) {
+
+        }
+
+
+        // Implement your booking logic here
+    }
 
     useEffect(() => {
         if (!user) {
@@ -38,7 +56,15 @@ const BookNow = () => {
                     console.error('Error fetching user profile:', error);
                 });
         }
-    }, [user, setUser]);
+
+        // Check if fromDate and toDate are in valid date formats
+        const isValidFromDate = moment(fromDate, 'MM-DD-YYYY', true).isValid();
+        const isValidToDate = moment(toDate, 'MM-DD-YYYY', true).isValid();
+
+        if (!isValidFromDate || !isValidToDate) {
+            setIsValidDates(false);
+        }
+    }, [user, setUser, fromDate, toDate]);
 
     return (
         <div>
@@ -46,8 +72,8 @@ const BookNow = () => {
             <div className="container">
                 {loading ? (
                     <Loader />
-                ) : error ? (
-                    <p>Error: {error.message}</p>
+                ) : !isValidDates ? (
+                    <Error />
                 ) : (
                     <div className="detailsWrapper">
                         <div className="imageContainer">
@@ -57,17 +83,17 @@ const BookNow = () => {
                         <div className="details">
                             <h1>Booking Detail:</h1>
                             {/* <h2 className='name'>Name: {user.name}</h2> */}
-                            <p className='name'>Name: {user && user.name}</p>
-                            <p className='name'>Room: {data.name} </p>
-                            <p className='name'>Branch: {data.branch} </p>
-                            <p className='fromDate'>From Date: <span>{fromDate}</span></p>
-                            <p className='toDate'>To Date: <span>{toDate}</span></p>
-                            <p className='maxCount'>Max Count: <span>{data.maxPeople}</span></p>
+                            <h2 className='name'>Name: {user && user.name}</h2>
+                            <p className='name'><b>Room:</b> {data.name} </p>
+                            <p className='name'><b>Branch:</b> {data.branch} </p>
+                            <p className='fromDate'><b>From Date:</b> <span>{fromDate}</span></p>
+                            <p className='toDate'><b>To Date:</b> <span>{toDate}</span></p>
+                            <p className='maxCount'><b>Max People:</b> <span>{data.maxPeople}</span></p>
                             <h1>Amount</h1>
-                            <p className="totalDays">Total Days: <span>{totalDays}</span></p>
-                            <p className="totalDays">Rent Per Day: {data.price}</p>
+                            <p className="totalDays"><b>Total Days:</b> <span>{totalDays} day/s</span></p>
+                            <p className="totalDays"><b>Rent Per Day:</b> ₱{data.price}</p>
                             <h1>Total Amount: ₱{totalAmount}</h1>
-                            <Link to={`/payment/${id}`} className='payBtn'>Book Now</Link>
+                            <Link className='payBtn' onClick={bookRoom}>Book Now</Link>
 
                         </div>
                     </div>
