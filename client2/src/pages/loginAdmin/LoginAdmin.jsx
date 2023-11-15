4b30ec05f9a7e6ea3e4cc4393a8e784678de7a1e
ramonPL14 <2101102020@student.buksu.edu.ph>
2023-11-15 17:29:27 +0800

    loginAdmin page

--- client2/src/pages/loginAdmin/LoginAdmin.jsx
+++ client2/src/pages/loginAdmin/LoginAdmin.jsx
@@ -5,10 +5,19 @@ import Sidebar from '../../components/Sidebar';
 
 const LoginAdmin = () => {
     return (
-        <div className='loginAdmin' >
-            <div className='loginText'>Login As Admin</div>
-
-            <a href="../" className='back'>Back to Dashboard</a>
+        <div>
+            <div className="loginAdminContainer">
+                <h1 className='heading'>Login as Admin</h1>
+                <form action="../">
+                    <label htmlFor="">Username:</label>
+                    <input placeholder="Enter Username" type="text" name="" id="" />
+                    <label htmlFor="">Password:</label>
+                    <input placeholder="Enter Password" type="password" name="" id="" />
+                    <label htmlFor="">Confirm Password:</label>
+                    <input placeholder="Confirm Password" type="password" name="" id="" />
+                   <input type="submit" className='loginAdminBtn' value="Login"/>
+                </form>
+            </div>
         </div>
     )
 }
