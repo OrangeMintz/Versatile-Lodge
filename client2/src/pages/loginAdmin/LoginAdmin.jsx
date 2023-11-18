a97583f2aa2bc0fcb5e8ba6d393983fdb45ec197
ramonPL14 <2101102020@student.buksu.edu.ph>
2023-11-18 16:01:06 +0800

    edit adminLogin

--- client2/src/pages/loginAdmin/LoginAdmin.jsx
+++ client2/src/pages/loginAdmin/LoginAdmin.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './loginAdmin.css';
-import HeaderAdmin from '../../components/HeaderAdmin';
-import Sidebar from '../../components/Sidebar';
 
 const LoginAdmin = () => {
     return (
-        <div className="loginAdmin">
+        <div>
             <div className="loginAdminContainer">
                 <h1 className='heading'>Login as Admin</h1>
                 <form action="../">
@@ -13,8 +11,6 @@ const LoginAdmin = () => {
                     <input placeholder="Enter Username" type="text" name="" id="" />
                     <label htmlFor="">Password:</label>
                     <input placeholder="Enter Password" type="password" name="" id="" />
-                    <label htmlFor="">Confirm Password:</label>
-                    <input placeholder="Confirm Password" type="password" name="" id="" />
                    <input type="submit" className='loginAdminBtn' value="Login"/>
                 </form>
             </div>
