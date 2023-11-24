e5a4eaa17bddea76b07b680c2b5b8cac6946b5d1
OrangeMintz <Nickzgacus@gmail.com>
2023-11-24 23:29:04 +0800

    fixed issues on employees. added validations

--- server/routes/bookinghistory.js
+++ server/routes/bookinghistory.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createBookingHistory, deleteBookHistory, getBookHistory, getBookHistoryByUserId } = require('../controllers/bookHistoryContr.js');
+const { createBookingHistory, deleteBookHistory, getBookHistory, getBookHistoryByUserId, updateBookingHistory } = require('../controllers/bookHistoryContr.js');
 
 const BookingHistory = require("../models/BookingHistory.js");
 // const { verifyAdmin, verifyUser } = require('../utils/verifyToken.js');
@@ -8,6 +8,9 @@ const router = express.Router();
 
 //Create
 router.post("/", createBookingHistory);
+
+//Update
+router.put("/:id", updateBookingHistory);
 
 //Delete
 router.delete("/:id", deleteBookHistory);
