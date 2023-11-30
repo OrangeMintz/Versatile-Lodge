0b4853ae8f583aef5f1566a9756937ecc57aba3a
OrangeMintz <Nickzgacus@gmail.com>
2023-12-01 00:44:09 +0800

    done with Admin Reply

--- client2/src/pages/reviewsAdmin/ReviewsAdmin.jsx
+++ client2/src/pages/reviewsAdmin/ReviewsAdmin.jsx
@@ -85,9 +85,15 @@ const ReviewsAdmin = () => {
     }, []);
 
 
-    //Reply
+    //REPLY
+    const MAX_CHARACTERS_TO_SHOW = 50;
+
     const [replyInputs, setReplyInputs] = useState({});
     const [showReplyInput, setShowReplyInput] = useState({});
+    const [expandedReviewIds, setExpandedReviewIds] = useState({});
+
+
+
 
 
     const handleCancelReply = (reviewId) => {
@@ -104,88 +110,180 @@ const ReviewsAdmin = () => {
 
     const handleReply = async (reviewId, event) => {
         try {
+            const { id, name, email, image } = user; // Get user details
+            const replyText = replyInputs[reviewId];
 
+            if (replyText) {
+                // Send a POST request to create a reply
+                const response = await axios.post(`/api/reviews/${reviewId}/replies`, {
+                    user_id: id,
+                    user_name: name,
+                    user_email: email,
+                    user_image: image,
+                    reply: replyText,
+                });
 
+                // Update the reviews state with the new data
+                setReviews((prevReviews) =>
+                    prevReviews.map((review) =>
+                        review._id === reviewId ? response.data : review
+                    )
+                );
+
+                // Clear the reply input value
+                setReplyInputs((prevInputs) => ({ ...prevInputs, [reviewId]: '' }));
+                // Hide the reply input and button
+                setShowReplyInput((prev) => ({ ...prev, [reviewId]: false }));
+            } else {
+                // Handle empty reply text
+                toast.error('Please enter a reply.');
+            }
         } catch (error) {
             console.error('Error sending reply:', error);
-
+            toast.error('Error sending reply. Please try again.');
         }
     };
 
-    // admin static reply
-    const adminReply = "Thank you for your feedback! We appreciate your input.";
+
+    const handleDeleteReply = async (reviewId, replyId) => {
+        try {
+            // Send a DELETE request to delete a reply
+            await axios.delete(`/api/reviews/${reviewId}/replies/${replyId}`);
+
+            // Update the reviews state without the deleted reply
+            setReviews((prevReviews) =>
+                prevReviews.map((review) => {
+                    if (review._id === reviewId) {
+                        return {
+                            ...review,
+                            replies: review.replies.filter((reply) => reply._id !== replyId),
+                        };
+                    }
+                    return review;
+                })
+            );
+
+            toast.success('Reply deleted successfully');
+        } catch (error) {
+            console.error('Error deleting reply:', error);
+            toast.error('Error deleting reply. Please try again.');
+        }
+    };
+
+    const handleShowMoreReplies = (reviewId) => {
+        setExpandedReviewIds((prev) => ({
+            ...prev,
+            [reviewId]: !prev[reviewId],
+        }));
+    };
 
     return (
-            <div className='reviewsPage'>
-                <HeaderAdmin />
-                <Sidebar />
+        <div className='reviewsPage'>
+            <HeaderAdmin />
+            <Sidebar />
 
-                <section className="reviews">
-                    <h1 className="heading">Customer's Reviews</h1>
+            <section className="reviews">
+                <h1 className="heading">Customer's Reviews</h1>
 
-                    <div className="box-container">
-                        {loading && <Loader />}
-                        {error && <Error />}
+                <div className="box-container">
+                    {loading && <Loader />}
+                    {error && <Error />}
 
-                        {!loading &&
-                            !error &&
-                            reviews.map((review) => (
-                                <div className="box" key={review._id}>
-                                    <div className="customer">
-                                        <img src={review.image} alt="" />
-                                        <div>
-                                            <h3>{review.name}</h3>
-                                            <span>{moment(review.date).format('MMMM DD YYYY HH:mm:ss')}</span>
-                                        </div>
-                                    </div>
-                                    <p>{review.comment}</p>
-                                    <div className="reply-container">
-                                        {showReplyInput[review._id] && (
-                                            <div className='replyInputAndBtn'>
-                                                <input
-                                                    className='replyInput'
-                                                    type="text"
-                                                    placeholder="Type your reply..."
-                                                    value={replyInputs[review._id] || ''}
-                                                    onChange={(e) => updateReplyInput(review._id, e.target.value)}
-                                                />
-                                                <div className='replyBtns'>
-                                                    <button className="reply-btn1" onClick={() => handleReply(review._id)}>
-                                                        Reply
-                                                    </button>
-                                                    <button className="reply-btn2" onClick={() => handleCancelReply(review._id)}>
-                                                        Cancel
-                                                    </button>
-                                                </div>
-                                        </div>
-                                        )}
-                                        {!showReplyInput[review._id] && (
-                                            <button className="reply-btn2" onClick={() => setShowReplyInput((prev) => ({ ...prev, [review._id]: true }))}>
-                                                Reply
-                                            </button>
-                                        )}
-                                        {/* Display admin's static reply */}
-                                        <div className='replyContent'>
-                                            {
-                                                adminReply && (
-                                                <div className="admin-reply">
-                                                    <p>{adminReply}</p>
-                                                    <button>Delete</button>
-                                                </div>
-                                            )}  
-                                        </div>
+                    {!loading &&
+                        !error &&
+                        reviews.map((review) => (
+                            <div className="box" key={review._id}>
+                                <div className="customer">
+                                    <img src={review.image} alt="" />
+                                    <div>
+                                        <h3>{review.name}</h3>
+                                        <span>{moment(review.date).format('MMMM DD YYYY HH:mm:ss')}</span>
                                     </div>
                                 </div>
-                            ))}
-                            
-                    </div>
+                                <p>{review.comment}</p>
+                                <div className="reply-container">
+                                    {showReplyInput[review._id] && (
+                                        <div className='replyInputAndBtn'>
+                                            <input
+                                                className='replyInput'
+                                                type="text"
+                                                placeholder="Type your reply..."
+                                                value={replyInputs[review._id] || ''}
+                                                onChange={(e) => updateReplyInput(review._id, e.target.value)}
+                                            />
+                                            <div className='replyBtns'>
+                                                <button className="reply-btn1" onClick={() => handleReply(review._id)}>
+                                                    Reply
+                                                </button>
+                                                <button className="reply-btn2" onClick={() => handleCancelReply(review._id)}>
+                                                    Cancel
+                                                </button>
+                                            </div>
+                                        </div>
+                                    )}
+                                    {!showReplyInput[review._id] && (
+                                        <button className="reply-btn2" onClick={() => setShowReplyInput((prev) => ({ ...prev, [review._id]: true }))}>
+                                            Reply
+                                        </button>
+                                    )}
+                                    {review.replies.length > 0 && (
+                                        <>
+                                            {review.replies.slice(0, expandedReviewIds[review._id] ? review.replies.length : 1).map((reply, index) => (
+                                                <div key={reply._id} className={`replyContent ${review._id === expandedReviewIds ? '' : 'hidden-reply'}`}>
+                                                    <div className="customer">
+                                                        <img src={reply.user_image} alt="" />
+                                                        <div>
+                                                            <h3>{reply.user_name}</h3>
+                                                            <span>{moment(reply.date).format('MMMM DD YYYY HH:mm:ss')}</span>
+                                                        </div>
+                                                    </div>
+                                                    <div className="admin-reply">
+                                                        <p style={{ marginBottom: "20px" }}>
+                                                            {reply.reply.length > MAX_CHARACTERS_TO_SHOW
+                                                                ? (
+                                                                    <>
+                                                                        {reply.expanded ? reply.reply : `${reply.reply.substring(0, MAX_CHARACTERS_TO_SHOW)}...`}
+                                                                        <span
+                                                                            style={{ marginLeft: '5px', cursor: 'pointer', fontSize: "1em" }}
+                                                                            onClick={() => {
+                                                                                const updatedReplies = [...review.replies];
+                                                                                updatedReplies[index].expanded = !updatedReplies[index].expanded;
+                                                                                setReviews((prevReviews) => prevReviews.map((prevReview) => (prevReview._id === review._id ? { ...prevReview, replies: updatedReplies } : prevReview)));
+                                                                            }}
+                                                                        >
+                                                                            {reply.expanded ? 'Show Less' : 'Show More'}
+                                                                        </span>
+                                                                    </>
+                                                                )
+                                                                : reply.reply
+                                                            }
+                                                        </p>
+                                                        <button onClick={() => handleDeleteReply(review._id, reply._id)}>Delete</button>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                            {review.replies.length > 1 && (
+                                                <button
+                                                    style={{ marginTop: "15px", borderRadius: "5px", padding: "10px" }}
+                                                    onClick={() => handleShowMoreReplies(review._id)}
+                                                >
+                                                    {expandedReviewIds[review._id] ? 'Show Less Replies' : 'Show More Replies'}
+                                                </button>
+                                            )}
+                                        </>
+                                    )}
+                                </div>
+                            </div>
+                        ))}
+
+                </div>
 
 
-                </section>
+            </section>
 
-                <Footer />  
+            <Footer />
 
-            </div>
+        </div>
     )
 }
 
