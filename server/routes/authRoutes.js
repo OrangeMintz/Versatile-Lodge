1a1f21ec17711ba732d4dc13df0096c17425b2b5
OrangeMintz <Nickzgacus@gmail.com>
2023-12-13 23:31:00 +0800

    cleaned all unnecessary files and added authentication for user and admin

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,20 +3,31 @@ const router = express.Router();
 const { registerUser, loginUser, getProfile, getAdminProfile, updateUser, updatePassword, getUser, getUsers } = require('../controllers/authController');
 const authenticateUser = require('../middleware/authMiddleware');
 
-
+//Create
 router.post('/register/customer', registerUser)
+
+//Update User
 router.put('/update/:id', authenticateUser, updateUser)
+
+//Update Password
 router.put('/updatepassword/:id', authenticateUser, updatePassword)
+
+//Login User
 router.post('/login/customer', loginUser)
+
+//Get User Token 
 router.get('/profile/', getProfile)
+
+//Get Admin Token
 router.get('/profile/admin', getAdminProfile)
 
+//Get Specific
 router.get("/customer/:id", getUser);
+
+//Get All
 router.get("/customer/", getUsers);
 
-
-
-
+//Logout
 router.get('/logout', (req, res) => {
     res.clearCookie('token');
     res.status(200).json({ message: 'Logout successful' });
